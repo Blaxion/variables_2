dd04f4e088605cfa553f2c399e44bbcf4965b4cb
Blaxion <khibaziev.f@gmail.com>
2021-04-15 14:15:32 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// Concaténation et addition
+let stringo_a1 = "J'aime les tartines et j'en mange";
+console.log(stringo_a1);
+
+let stringo2 ="4 par jours"
+console.log(stringo2);
+
+console.log(stringo_a1,stringo2);
+console.log(stringo_a1+" "+stringo2);
+let stringofusion = stringo_a1+" "+stringo2;
+console.log(stringofusion);
+
+let integro_a1 = 5;
+let integro_a2 = 35;
+console.log(integro_a1+integro_a2)
+
+console.log(stringo_a1,integro_a1)
+console.log(stringo_a1+" "+integro_a1)
+
+// La réaffectation
+let stringo_b1 = "Hello World";
+console.log(stringo_b1);
+
+let stringo_b2 = "Bye bye";
+console.log(stringo_b2);
+
+stringo_b1 = stringo_b2;
+console.log(stringo_b1);
+
+// Grandeurs, Egalites, Différence et Booleans
+
+let integro_c1=4;
+let stringo_c1="6";
+
+// console.log(integro_c1 = stringo_c1);
+console.log(integro_c1 > stringo_c1);
+console.log(integro_c1 < stringo_c1);
+console.log(integro_c1 == stringo_c1);
+console.log(integro_c1 != stringo_c1);
+console.log(integro_c1 === stringo_c1);
+console.log(integro_c1 !== stringo_c1);
+
+
+
